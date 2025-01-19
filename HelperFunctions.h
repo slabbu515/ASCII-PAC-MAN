473b0c0f8dddfaa227bc7519b9cbec342a8d2498
slabbu515 <popodim515@gmail.com>
2025-01-19 20:29:59 +0200

    Added a function to get a character position from the map.

---
+++ HelperFunctions.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Point.h"
+#include "Map.h"
+
+Point getCharacterPosition(const Map& map, char ch)
+{
+	Point charPos{ -1, -1 };
+
+	for (int i = 0; i < map.height; i++)
+	{
+		for (int j = 0; j < map.width; j++)
+		{
+			if (map.contents[i][j] == ch)
+			{
+				charPos.x = i;
+				charPos.y = j;
+				return charPos;
+			}
+		}
+	}
+	return charPos;
+}
