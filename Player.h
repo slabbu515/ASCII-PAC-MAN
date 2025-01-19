da56c428de10cc25f07162f9529b69289d7c5ae0
slabbu515 <popodim515@gmail.com>
2025-01-19 21:33:57 +0200

    Updated constants. Updated movePlayer function.

--- Player.h
+++ Player.h
@@ -1,18 +1,19 @@
 #pragma once
 #include <windows.h>
+#include "Constants.h"
 #include "Point.h"
 #include "Map.h"
 
 struct Player
 {
 	Point position;
-	const int colour = 14; //Colour code for yellow
-	const char symbol = 'C';
-	char movementDirection = 'A';
+	const int colour = PLAYER_COLOUR;
+	const char symbol = PLAYER_SYMBOL;
+	char movementDirection = MOVEMENT_LEFT;
 };
 
 bool initializePlayer(Player& player, const Map& map);
 void printPlayer(const Player& player, const HANDLE& consoleHandle);
-void movePlayer(Player& player, const Map& map);
+bool movePlayer(Player& player, const Map& map);
 void changeDirection(Player& player, char newDirection);
 
