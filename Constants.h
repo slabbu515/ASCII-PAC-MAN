158ceddebc0e56b8f197ff8f5f68aa2457e388f6
slabbu515 <popodim515@gmail.com>
2025-01-19 21:32:36 +0200

    Added new constants.

--- Constants.h
+++ Constants.h
@@ -1,2 +1,13 @@
 #pragma once
+
 const int DEFAULT_CONSOLE_COLOUR = 15; //White
+
+const char WALL_CHARACTER = '#';
+
+const int PLAYER_COLOUR = 14; //Yellow
+const char PLAYER_SYMBOL = 'C';
+
+const char MOVEMENT_LEFT = 'A';
+const char MOVEMENT_RIGHT = 'D';
+const char MOVEMENT_UP = 'W';
+const char MOVEMENT_DOWN = 'S';
