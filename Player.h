13c196a8ecbb56e0ea6973921c347bd667675ad1
slabbu515 <popodim515@gmail.com>
2025-01-19 21:09:20 +0200

    Added colour attribute to Player. Changed symbol to const. Updated printPlayer declaration.

--- Player.h
+++ Player.h
@@ -1,16 +1,18 @@
 #pragma once
+#include <windows.h>
 #include "Point.h"
 #include "Map.h"
 
 struct Player
 {
 	Point position;
-	char symbol = 'C';
+	const int colour = 14; //Colour code for yellow
+	const char symbol = 'C';
 	char movementDirection = 'A';
 };
 
 bool initializePlayer(Player& player, const Map& map);
-void printPlayer(const Player& player);
+void printPlayer(const Player& player, const HANDLE& consoleHandle);
 void movePlayer(Player& player, const Map& map);
 void changeDirection(Player& player, char newDirection);
 
