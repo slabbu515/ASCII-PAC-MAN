18e8ed9e2d6a58929c93928bc4765087759786d8
slabbu515 <popodim515@gmail.com>
2025-01-19 21:56:23 +0200

    Added initializeMap function.

--- Map.cpp
+++ Map.cpp
@@ -1,26 +1,37 @@
 #include <fstream>
+#include <iostream>
 #include "Map.h"
 #include "Constants.h"
 #include "Point.h"
 
 using namespace std;
+
+void initializeMap(Map& map)
+{
+	cin >> map.height >> map.width;
+	map.contents = new char* [map.height + 1] {nullptr};
+	for (int i = 0; i < map.height; i++)
+	{
+		map.contents[i] = new char[map.width + 1] {'\0'};
+	}
+}
 bool loadMapFromFile(Map& map)
 {
-    ifstream ifs(MAP_FILE_NAME);
-    if (!ifs.is_open())
-    {
-        return false;
-    }
+	ifstream ifs(MAP_FILE_NAME);
+	if (!ifs.is_open())
+	{
+		return false;
+	}
 
-    for (int i = 0; i < map.height; i++)
-    {
-        ifs.getline(map.contents[i], map.width);
-    }
+	for (int i = 0; i < map.height; i++)
+	{
+		ifs.getline(map.contents[i], map.width);
+	}
 
-    return true;
+	return true;
 }
 
 bool canMoveOn(const Map& map, const Point& position)
 {
-    return map.contents[position.x][position.y] != WALL_CHARACTER;
+	return map.contents[position.x][position.y] != WALL_CHARACTER;
 }
--- Map.h
+++ Map.h
@@ -7,5 +7,6 @@ struct Map
 	int height;
 };
 
+void initializeMap(Map& map);
 bool loadMapFromFile(Map& map);
 bool canMoveOn(const Map& map, const Point& position);
