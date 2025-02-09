c7a52581cc402bf0cd1a2fa2a2f16aca4f30e113
slabbu515 <popodim515@gmail.com>
2025-02-09 13:22:29 +0200

    Added new constants.

--- Constants.h
+++ Constants.h
@@ -5,9 +5,12 @@ const int DEFAULT_CONSOLE_COLOUR = 15; //White
 const char MAP_FILE_NAME[] = "map.txt";
 
 const char WALL_CHARACTER = '#';
+const char POINT_CHARACTER = '-';
+const char ENERGIZER_CHARACTER = '@';
 
 const int PLAYER_COLOUR = 14; //Yellow
 const char PLAYER_SYMBOL = 'C';
+
 const char BLANK = ' ';
 
 const int ALL_ENTITIES_COUNT = 1;
