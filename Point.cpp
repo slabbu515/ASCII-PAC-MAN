2df26d6ad25f439c384b65cd29a8ee405e2e4f0e
slabbu515 <popodim515@gmail.com>
2025-02-09 17:36:27 +0200

    Added squaredDistance function and areCoincident overload.

--- Point.cpp
+++ Point.cpp
@@ -4,3 +4,15 @@ bool areCoincident(const Point& point, int x, int y)
 {
     return point.x == x && point.y == y;
 }
+
+bool areCoincident(const Point& pointA, const Point& pointB)
+{
+    return pointA.x == pointB.x && pointA.y == pointB.y;
+}
+
+int squaredDistance(const Point& pointA, const Point& pointB)
+{
+    int xDiff = pointB.x - pointA.x;
+    int yDiff = pointB.y - pointA.y;
+    return xDiff * xDiff + yDiff * yDiff;
+}
--- Point.h
+++ Point.h
@@ -7,3 +7,5 @@ struct Point
 };
 
 bool areCoincident(const Point& point, int x, int y);
+bool areCoincident(const Point& pointA, const Point& pointB);
+int squaredDistance(const Point& pointA, const Point& pointB);
