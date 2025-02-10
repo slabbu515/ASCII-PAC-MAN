f802c05c3ba77033479d626ccb6943b76e94fe6a
slabbu515 <popodim515@gmail.com>
2025-02-10 19:02:26 +0200

    Created getReversedVector function.

--- Point.cpp
+++ Point.cpp
@@ -16,3 +16,11 @@ int squaredDistance(const Point& pointA, const Point& pointB)
     int yDiff = pointB.y - pointA.y;
     return xDiff * xDiff + yDiff * yDiff;
 }
+
+Point getReversedVector(const Point& pointA, const Point& pointB)
+{
+    Point revVector;
+    revVector.x = pointA.x - pointB.x;
+    revVector.y = pointA.y - pointB.y;
+    return revVector;
+}
--- Point.h
+++ Point.h
@@ -9,3 +9,4 @@ struct Point
 bool areCoincident(const Point& point, int x, int y);
 bool areCoincident(const Point& pointA, const Point& pointB);
 int squaredDistance(const Point& pointA, const Point& pointB);
+Point getReversedVector(const Point& pointA, const Point& pointB);
